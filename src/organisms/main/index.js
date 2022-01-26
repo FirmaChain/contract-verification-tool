bf232801b911a7684377f4c82baa5c63a9c49998
chill-beer <juno87210@gmail.com>
2022-01-26 23:14:09 +0900

    fix: main screen layout fixed for desktop

--- src/organisms/main/index.js
+++ src/organisms/main/index.js
@@ -3,12 +3,13 @@ import { Box, DescText, StyledLink } from 'components/styles';
 import Cards from './cards';
 import RectButton from 'components/button/rectButton';
 import { MAIN_DESC } from 'constants/texts';
+import { isDesktop } from 'react-device-detect';
 
 export default function Main() {
     return (
         <Box
             direction={"column"}>
-            <DescText style={{padding: "16px 0 0"}}>{MAIN_DESC}</DescText>
+            <DescText isDesktop={isDesktop} style={{padding: "16px 0 0"}}>{MAIN_DESC}</DescText>
             <Cards />
             <StyledLink to="/upload">
                 <RectButton title="VERIFICATION"/>
